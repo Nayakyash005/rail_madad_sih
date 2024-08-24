112abb609663da6471d2c104b6c14a7a730917e6
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-08-24 18:57:44 +0530

    :-)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import { Outlet } from "react-router-dom";
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
 
-function App() {
+import LoadingSpinner from "./components/LoadingSpinner";
+import HomeLayout from "./pages/home/Layout";
+import Home from "./pages/home";
+
+import AdminLayout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <LoadingSpinner />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AdminLayout />,
+
+    children: [
+      {
+        path: "/admin",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+
+export default function App() {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import LoadingSpinner from "./components/LoadingSpinner";
-import Home from "./pages/home";
-import Dashboard from "./pages/admin/Dashboard";
-import Layout from "./pages/admin/Layout";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "home",
-        element: <LoadingSpinner />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <Layout />,
-
-    children: [
-      {
-        path: "/admin",
-        element: <Dashboard />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <App/>
 );
